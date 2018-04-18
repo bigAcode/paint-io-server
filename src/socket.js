f4b0009ecd0520ab6c351ae78cac90fc3202996b
Ben Linville <linvilb@amazon.com>
2018-04-18 11:19:31 -0600

    Get Message, Broadcast

--- src/socket.js
+++ src/socket.js
@@ -1,25 +1,29 @@
 const db = require('./db');
-
+const DRAW_POINTS_EVENT = "DRAW_POINTS";
 const onSocketConnect = io => socket => {
 
-  // TODO 2.1 Listen for login events (eg "LOGIN") from client and save the user using db.create(username, socket.id)
-  // TODO 2.2 Prevent users from using an existing username using the "acknowledgement" from the client
-  // TODO 2.3 Emit an update user list event (eg "UPDATE_USER_LIST") to all clients when there is a login event
-  // TODO 2.4 Listen for "disconnect" events and remove the socket user from the users object (*hint: db.create(username, socket.id) returns the logout fn)
-  // TODO 2.5 emit "UPDATE_USER_LIST" after user has been "logged out" and is removed from "users" object
+    // TODO 2.1 Listen for login events (eg "LOGIN") from client and save the user using db.create(username, socket.id)
+    // TODO 2.2 Prevent users from using an existing username using the "acknowledgement" from the client
+    // TODO 2.3 Emit an update user list event (eg "UPDATE_USER_LIST") to all clients when there is a login event
+    // TODO 2.4 Listen for "disconnect" events and remove the socket user from the users object (*hint: db.create(username, socket.id) returns the logout fn)
+    // TODO 2.5 emit "UPDATE_USER_LIST" after user has been "logged out" and is removed from "users" object
 
-  // TODO 3.1 Check if a "toUser" is specified and only broadcast to that user
-  // TODO 3.2 Include information about the "fromUser" so the client can filter draw events from other users and only display events from the selected user
+    // TODO 3.1 Check if a "toUser" is specified and only broadcast to that user
+    // TODO 3.2 Include information about the "fromUser" so the client can filter draw events from other users and only display events from the selected user
 
-  // TODO 1.4 listen for draw action-type events (eg "DRAW_POINTS") from the socket and broadcast them to others sockets.
-
+    // TODO 1.4 listen for draw action-type events (eg "DRAW_POINTS") from the socket and broadcast them to others sockets.
+    socket.on(DRAW_POINTS_EVENT, ({points, color}) => {
+        socket.broadcast.emit(DRAW_POINTS_EVENT, {points, color});
+    });
 };
 
 const connect = server => {
-  // TODO 1.1 import socket.io
-  // TODO 1.2 attach a socket to the express server by passing the express server instance as an argument when socket.io is invoked
+    // TODO 1.1 import socket.io
+    const io = require("socket.io")(server);
+    // TODO 1.2 attach a socket to the express server by passing the express server instance as an argument when socket.io is invoked
 
-  // TODO 1.3 listen for new connections and use the provided "onSocketConnect" function
+    // TODO 1.3 listen for new connections and use the provided "onSocketConnect" function
+    io.on('connect', onSocketConnect(io));
 }
 
 module.exports = connect;
